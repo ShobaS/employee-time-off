6f94ad6342c00fd0fd3533a64e932eac0194fb5c
Shoba Selvaraj <shobaselvaraj.95@gmail.com>
2021-08-23 20:04:40 +0530

    custom calendar functionality added

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calendar from './components/calendar/calendar.js'
+const App = () => {
+  return (
+    <div className="App">
+      <main>
+        <Calendar />
+      </main>
+    </div>
+  );
+}
+
+export default App;
